6a6684d7c82803083cd756ec876abe1041b528ce
GangHyeok Lee <rkdgur5381@gmail.com>
2018-10-10 10:46:44 +0900

    1010_Chapter5 echostate code : check echo mod
    Signed-off-by: GangHyeok Lee <rkdgur5381@gmail.com>

---
+++ echostate.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <termios.h>
+#include <stdlib.h>
+
+void main()
+{
+	struct termios info;
+	int rv;
+
+	rv = tcgetattr(0, &info);
+
+	if(rv == -1)
+	{
+		perror("tcgetattr");
+		exit(1);
+	}
+
+	if(info.c_lflag & ECHO)
+	{
+		printf(" echo is on, since its bit is 1\n");
+	}
+	else
+	{
+		printf(" echo if OFF, since its bit is 0\n");
+	}
+
+
+}
