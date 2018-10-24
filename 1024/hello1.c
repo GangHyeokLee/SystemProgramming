33dc2630d16095541a6ee926366a77494473de20
GangHyeok Lee <rkdgur5381@gmail.com>
2018-10-24 12:21:52 +0900

    Chapter7 hello code(게임 만들기 직전 코드)
    Signed-off-by: GangHyeok Lee <rkdgur5381@gmail.com>

---
+++ 1024/hello1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <curses.h>
+
+void main()
+{
+	initscr();
+
+	clear();
+	move(10,20);
+	addstr("Hello, world");
+	move(LINES-1, 0);
+
+	refresh();
+	getch();
+
+	endwin();
+}
+
