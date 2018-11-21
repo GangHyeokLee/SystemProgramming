957fd67a3ecd4b572c4ec5cf50608217d5363bec
GangHyeok Lee <rkdgur5381@gmail.com>
2018-11-21 09:41:35 +0900

    Homework6 Final version
    Signed-off-by: GangHyeok Lee <rkdgur5381@gmail.com>

--- hw6/hw6_2017111978.c
+++ hw6/hw6_2017111978.c
@@ -19,36 +19,25 @@ void main(int argc, char *argv[])
     if(pipe(thepipe[1]) == -1)
         oops("Cannot get a second pipe", 1);
 
-    printf("%d %d %d %d\n", thepipe[0][0], thepipe[0][1], thepipe[1][0], thepipe[1][1]);
-
-    if(getpid() == parentpid) //make two child
+    if(getpid() == parentpid) //make first child
     {
         if((pid = fork()) == -1)
         {
             oops("Cannot fork1", 2);
         }
-        else
-        {
-            printf("fork1 succeed\n");
-        }
     }
-
-    if(getpid() == parentpid)
+    if(getpid() == parentpid) //make second child
     {
         if((pid = fork()) == -1)
         {
             oops("Cannot fork2", 3);
         }
-        else
-        {
-            printf("fork2 succeed\n");
-        }
     }
+
+
 
     if(getpid() == parentpid) // parent will execute argv[3]
     {
-        printf("%d\n", getpid());
-        printf("parent start\n");
         close(thepipe[1][1]); //close writing end of second pipe
 
         //close irrelevant pipe
@@ -64,8 +53,6 @@ void main(int argc, char *argv[])
     }
     else if(getpid() == (parentpid+1)) //first child will execute argv[2]
     {
-        printf("%d\n", getpid());
-        printf("child1 start\n");
         close(thepipe[1][0]); //close reading end of second pipe
         close(thepipe[0][1]); //close writing end of first pipe
 
@@ -73,20 +60,16 @@ void main(int argc, char *argv[])
             oops("Could not redirect stdout", 6);
 
         if(dup2(thepipe[0][0], 0) == -1) //duplicate reading end of first pipe to stdin
-            oops("Could not redirect stdout", 7);
-
+            oops("Could not redirect stdin", 7);
 
         close(thepipe[1][1]);
         close(thepipe[0][0]);
 
-
-        execlp(argv[2], argv[1], NULL);
+        execlp(argv[2], argv[2], NULL);
         oops(argv[2], 8);
     }
     else if(getpid() == (parentpid+2)) //second child will execute argv[1]
     {
-        printf("%d\n", getpid());
-        printf("child2 start\n");
         close(thepipe[0][0]); //close reading end of first pipe
 
         //close irrelevant pipe
@@ -98,7 +81,7 @@ void main(int argc, char *argv[])
 
         close(thepipe[0][1]); //close free pipe
 
-        execlp(argv[3], argv[3], NULL);
+        execlp(argv[1], argv[1], NULL);
         oops(argv[3],10);
     }
     return;
